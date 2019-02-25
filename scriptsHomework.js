d85aebc082171d40b18607f8f54cae5e4535d625
christopher cahill and robert lee <robert@>
2019-02-25 11:42:37 -0800

    add switching between songs with lyrics resetting to 0 index

--- scriptsHomework.js
+++ scriptsHomework.js
@@ -28,7 +28,6 @@ const initialState = {
 // REDUCER WILL GO songChangeReducer
 const lyricChangeReducer = (state = initialState.songsById, action) => {
 
-
   // Declares several variables used below, without yet defining.
   let newArrayPosition;
   let newSongsByIdEntry;
@@ -81,6 +80,25 @@ const lyricChangeReducer = (state = initialState.songsById, action) => {
   }
 }
 
+const songChangeReducer = (state = initialState.currentSongId, action) => {
+  switch (action.type) {
+    case 'CHANGE_SONG':
+      return action.newSelectedSongId;
+    default:
+      return state;
+  }
+}
+
+const rootReducer = this.Redux.combineReducers({
+  currentSongId: songChangeReducer,
+  songsById: lyricChangeReducer,
+});
+
+// REDUX STORE
+const { createStore } = Redux;
+const store = createStore(rootReducer);
+console.log(store.getState());
+
 // JEST TESTS + SETUP
 const { expect } = window;
 
@@ -120,37 +138,99 @@ expect(lyricChangeReducer(initialState.songsById, { type: 'RESTART_SONG', curren
   }
 });
 
+expect(rootReducer(initialState, { type: null })).toEqual(initialState);
 
-// REDUX STORE
-const { createStore } = Redux;
-const store = createStore(lyricChangeReducer);
-console.log(store.getState());
+expect(store.getState().currentSongId).toEqual(songChangeReducer(undefined, { type: null }));
+expect(store.getState().songsById).toEqual(lyricChangeReducer(undefined, { type: null }));
 
-// // RENDERING STATE IN DOM
-// const renderLyrics = () => {
-//   const lyricsDisplay = document.getElementById('lyrics');
-//   while (lyricsDisplay.firstChild) {
-//     lyricsDisplay.removeChild(lyricsDisplay.firstChild);
-//   }
-//   const currentLine = store.getState().songLyricsArray[store.getState().arrayPosition];
-//   const renderedLine = document.createTextNode(currentLine);
-//   document.getElementById('lyrics').appendChild(renderedLine);
-// }
-//
-// window.onload = function() {
-//   renderLyrics();
-// }
-//
-// // CLICK LISTENER
-// const userClick = () => {
-//   const currentState = store.getState();
-//   if (currentState.arrayPosition === currentState.songLyricsArray.length - 1) {
-//     store.dispatch({ type: 'RESTART_SONG'});
-//   } else {
-//     store.dispatch({type: 'NEXT_LYRIC'});
-//   }
-//   console.log(store.getState());
-// }
-//
-// //SUBSCRIBE TO THE REDUX STORE
-// store.subscribe(renderLyrics);
+// RENDERING STATE IN DOM
+const renderLyrics = () => {
+  const lyricsDisplay = document.getElementById('lyrics');
+  while (lyricsDisplay.firstChild) {
+    lyricsDisplay.removeChild(lyricsDisplay.firstChild);
+  }
+  if (store.getState().currentSongId) {
+    const currentLine = document.createTextNode(store.getState().songsById[store.getState().currentSongId].songArray[store.getState().songsById[store.getState().currentSongId].arrayPosition]);
+    document.getElementById('lyrics').appendChild(currentLine);
+  } else {
+     const selectSongMessage = document.createTextNode("Select a song from the menu above to sing along!");
+     document.getElementById('lyrics').appendChild(selectSongMessage);
+  }
+}
+
+const renderSongs = () => {
+  console.log('renderSongs method successfully fired!');
+   console.log(store.getState());
+   // Retrieves songsById state slice from store:
+  const songsById = store.getState().songsById;
+   // Cycles through each key in songsById:
+  for (const songKey in songsById) {
+    // Locates song corresponding with each key, saves as 'song' constant:
+    const song = songsById[songKey];
+    // Creates <li>, <h3>, and <em> HTMl elements to render this song's
+    // information in the DOM:
+    const li = document.createElement('li');
+    const h3 = document.createElement('h3');
+    const em = document.createElement('em');
+    // Creates text node containing each song's title:
+    const songTitle = document.createTextNode(song.title);
+    // Creates text node containing each song's artist:
+    const songArtist = document.createTextNode(' by ' + song.artist);
+    // Adds songTitle text node to the <em> element we created 3 lines up:
+    em.appendChild(songTitle);
+    // Adds <em> element that now contains song title to <h3> element created
+    // 5 lines up:
+    h3.appendChild(em);
+    // Also adds songArtist text node created 2 lines up to <h3> element created
+    // 6 lines up:
+    h3.appendChild(songArtist);
+    // Adds click event listener to same  <h3> element, when this <h3> is clicked,
+    // an event handler called selectSong() will run, using song's ID as argument:
+    h3.addEventListener('click', function() {
+      selectSong(song.songId);
+    });
+    // Adds entire <h3> element to the <li> element created 11 lines above:
+    li.appendChild(h3);
+    // Appends this <li> element to the <ul> in index.html with a 'songs' ID:
+    document.getElementById('songs').appendChild(li);
+  }
+}
+
+window.onload = function() {
+  renderSongs();
+  renderLyrics();
+}
+
+// CLICK LISTENER
+const userClick = () => {
+  const currentState = store.getState();
+  if (store.getState().songsById[store.getState().currentSongId].arrayPosition === store.getState().songsById[store.getState().currentSongId].songArray.length - 1) {
+    store.dispatch({
+      type: 'RESTART_SONG',
+      currentSongId: store.getState().currentSongId });
+  } else {
+    store.dispatch({
+      type: 'NEXT_LYRIC',
+      currentSongId: store.getState().currentSongId });
+  }
+  console.log(store.getState());
+}
+
+const selectSong = (newSongId) => {
+  let action;
+  if (store.getState().currentSongId) {
+    action = {
+      type: 'RESTART_SONG',
+      currentSongId: store.getState().currentSongId,
+    }
+    store.dispatch(action);
+  }
+  action = {
+    type: 'CHANGE_SONG',
+    newSelectedSongId: newSongId,
+  }
+  store.dispatch(action);
+}
+
+//SUBSCRIBE TO THE REDUX STORE
+store.subscribe(renderLyrics);
